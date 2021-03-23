27dd6ac921576e876648d458c470b2585743edd7
frederni <frederni@stud.ntnu.no>
2021-03-23 15:02:11 +0100

    in the middle of doing displayThread

--- project.py
+++ project.py
@@ -110,6 +110,21 @@ class Session(object):
         self.c.execute("SELECT LAST_INSERT_ID()")
         return int(self.c.fetchall()[0][0])
 
+    def getUserInfo(self, UID, getName=False, getInstructor=False):
+        self.c.execute("SELECT DisplayName, Usertype FROM piazza_user WHERE UserID=%s", (UID, ))
+        tup = list(self.c.fetchall()[0]) # Cast to list so we may modify last item
+        tup[1] = tup[1] == 'instructor'
+        if getName and getInstructor:
+            return tup
+        elif getName:
+            return tup[0]
+        elif getInstructor:
+            return tup[1]
+        return None
+
+    def getFolderFromID(self, FID):
+        raise NotImplementedError
+
     def createThread(self):
         """
         Method for creating a thread. Asks for Thread title, tag, folder and content and ensures legal types.
@@ -133,7 +148,11 @@ class Session(object):
         
         self.c.execute("SELECT FolderID, FolderName FROM folder WHERE CourseID=%s", (self.course,))
         courseFolders = self.c.fetchall()
-        courseFolders = np.array(courseFolders).reshape(len(courseFolders),2)
+        courseFolders = np.array(courseFolders).reshape(len(courseFolders),2) 
+        # courseFolders puts the folderID and folderName in an array of type
+        # [id1, name1]
+        # [id2, name2]
+        # [...,  ... ]
         print("Select folder: (", end='')
         for f in courseFolders[:,1]: # This loop simply prints all folder names in current course
             print(f, end=', ')
@@ -152,11 +171,12 @@ class Session(object):
                 if folder not in courseFolders[:,1]:
                     print("Invalid folder name, try again.")
             question = input("Enter post message: ")
-            isAnon = int(input("Anonymous? (Y/N) ").lower() == "y") # Boolean casted as int
-            # TODO only allow anon posts if global settings are ok 
+            self.c.execute("SELECT AllowsAnonPosts from Course where CourseID = %s", (self.course, ))
+            if int(c.fetchall()[0][0]): # If global course settings allow anon posts
+                isAnon = int(input("Anonymous? (Y/N) ").lower() == "y") # Boolean casted as int
 
 
-        folderID = courseFolders[np.where(courseFolders==folder)[0][0], 0] # TODO fix this
+        folderID = courseFolders[np.where(courseFolders==folder)[0][0], 0]
         
         # Now that we have all data we need, we need to insert new tuples
         print(folderID)
@@ -185,18 +205,62 @@ class Session(object):
         self.db.connection.commit() # Required to make actual changes to database
 
     def search():
-        pass
+        keyword = input("Search for: ")
+        padded = '%' + keyword + '%'
+        self.c.execute("SELECT * FROM post WHERE Textfield LIKE %s", (keyword, ))
+        results = c.fetchall
+        if len(results) !=0:
+            print("Found", len(results), "matching posts")
+            displayThread(results)
+        else:
+            print("No results")
+        
+        
 
-    def displayThread():
-        # Forslag til hvordan vi kan displaye det:
-        # <ThreadTag> : <Title>
-        # Status: No replies (RED) / Student reply (GREEN) / Instructor reply (ORANGE)
-        # for Post in posts:
-        #   <Post.PostTag> | Posted by: <Interactwith.UserID>, time.ctime(<Interactwith.timestamp>)
-        #   <Post.Textfield>
-        #   print("-------")
-        # action = input("PostNo to like Post, 'r' to reply, 'n' to view next thread)
-        pass
+    def displayThread(results):
+        """
+        We display all posts in a thread with search match
+        """
+        for res in results: # results = ((val_11, val_12), (val_21, val-22), ...)
+            uniqueThreadIDs = list(set([res[i][1] for i in range(len(results))]))
+        for TID in uniqueThreadIDs:
+            self.c.execute("SELECT * FROM thread NATURAL JOIN post NATURAL JOIN interactwith WHERE TID=%s", (TID, ))
+            #[TID, PostNo, Title, ThreadTag, FolderID, IsAnonymous, PostTag, Textfield, UserID, Time_stamp, InteractionType]
+            #[ 0 ,   1   ,   2  ,     3    ,    4    ,    5       ,    6   ,    7     ,   8   ,   9       ,       10       ]
+            postTab = self.c.fetchall() # All posts belonging to thread
+            # We print the first post uniquely
+            title, tTag, timestamp, folder = postTab[0][2], postTab[0][3], time.ctime(postTab[0][9]), postTab[0][4]
+            print(ttag[0].upper() + ttag[1:], " : ", title, " (In folder ", self.getFolderFromID(folder),")", sep='') #TODO implement that fnc
+            
+            #Determine status (color) of thread
+            studentReply, instructorReply = False, False # Default bool flags to false that is used to color thread
+            for post in postTab:
+                if post[10] == 'answer' and self.getUserInfo(post[8], getInstructor=True): instructorReply = True
+                elif post[10] == 'answer': studentReply = True
+            statusStr = "Status: "
+            if studentReply: statusStr += "(GREEN)"
+            if instructorReply: statusStr += "(ORANGE)"
+            if not (studentReply and instructorReply): statusStr += "(RED)"
+            print(statusStr)
+
+            # Print all posts
+            for post in postTab: 
+                postnumber, isAnon, pTag, text, UID, timestamp, interType = post[1], post[5:]
+                timestamp = time.ctime(timestamp) # Convert to easily readable time
+                postedBy = self.getUserInfo(UID, getName=True)
+                print("#" + postnumber + "[" + pTag[0].upper() + pTag[1:] + "]", end=' ')
+                print("Posted by:", "Anonymous" if isAnon else postedBy, end=' ')
+                print(timestamp)
+                print(text)
+                print(15*'-')
+                # TODO consider implementing something to show amount of likes
+            action = input("enter post number followed by 'R' or 'L' to reply or like a post, e.g. '1R' or '2L'")
+            if action[1] == 'L':
+                self.like(TID=TID, postNo=int(action[0]))
+            elif action[1] == 'R':
+                self.reply(TID=TID, postNo=int(action[0]))
+            
+
 
     def viewStats(self):
         if not self.loginUser:
